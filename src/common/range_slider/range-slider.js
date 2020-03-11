e705fe141948be5598cb32979cc2b26ff70ad316
Яценко Андрей <andrejvmire@gmail.com>
2020-03-11 21:42:54 +0300

    переписан range-slider для любого кол-ва экземпляров на страницу

--- src/common/range_slider/range-slider.js
+++ src/common/range_slider/range-slider.js
@@ -1,27 +1,27 @@
 require("webpack-jquery-ui/slider");
 require("jquery");
 import currency from "../currency/currency";
-//todo: переписать для подключения произвольного кол-ва
+
 const RangeSlider = options => {
     let {className, classes} = options;
-    let dataset = $(className)[0].dataset;
-    //todo: передавать пределы и значения слайдера в объекте value. Приводить к числовому типу value.map(Number)!
-    let min = dataset.min * 1;
-    let max = dataset.max * 1;
-    let comment = $('#' + dataset.comment_id);
-    let slider = $(className).slider({
-        ...dataset,
-        min,
-        max,
-        classes,
-        range: true,
-        values: [dataset.from, dataset.to],
-        slide: function (event, ui) {
-            comment.html(currency({value: ui.values[0]}) + currency({value: ui.values[1]}));
-        }
-    });
-    comment.html(currency({value: $(className).slider("values", 0)}) +
-        currency({value: $(className).slider("values", 1)}));
+    $(className).each(function () {
+        let dataset = $(this)[0].dataset;
+        let {value} = dataset;
+        let {min, max, from, to} = JSON.parse(value);
+        let comment = $('#' + dataset.comment_id);
+        let slider = $(this).slider({
+            ...dataset,
+            min,
+            max,
+            classes,
+            range: true,
+            values: [from, to],
+            slide: (event, ui) => {
+                comment.html(currency({value: ui.values[0]}) + currency({value: ui.values[1]}));
+            }
+        })
+        comment.html(currency({value: from}) + currency({value: to}));
+    })
 }
 
 RangeSlider({
