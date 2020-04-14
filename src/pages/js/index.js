5d8ce416346a55ebbd87e451c7c54109e3a92ff2
Яценко Андрей <andrejvmire@gmail.com>
2020-04-14 23:20:09 +0300

    правки в carousel для разных режимов

--- src/pages/js/index.js
+++ src/pages/js/index.js
@@ -1,7 +1,7 @@
 import carousel from "../../common/carousel/carousel";
 import {uniformChoice} from "../../common/fieldset/__iqdropdown/iqdropdown";
 
-carousel('.carousel',{
+carousel('.carousel__background',{
     autoplay: true,
     dots: false,
     arrows: false,
--- src/pages/ui-kit/js/cards.js
+++ src/pages/ui-kit/js/cards.js
@@ -1,4 +1,7 @@
 import {uniformChoice} from "../../../common/fieldset/__iqdropdown/iqdropdown";
+import carousel from "../../../common/carousel/carousel";
 
 uniformChoice("roomFilter1");
 uniformChoice("roomSearch_1");
+
+carousel('.carousel__static');
